4f2b2b59f7f302da49f591a5c4ec7959409d58de
Takagenki <genkin611@gmail.com>
2019-08-22 17:44:51 +0900

    add ~/models/contest/question/question_output.go
    エラーハンドリングとコメントを追記

--- models/contest/question/question_output.go
+++ models/contest/question/question_output.go
@@ -7,22 +7,28 @@ import (
 )
 
 func main() {
+	// dbにアクセス
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/questionsdb")
+	// エラーハンドリング
+	err = db.Ping()
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
+	// SQLを実行
 	rows, err := db.Query("SELECT * FROM question")
 	defer rows.Close()
 	if err != nil {
 		panic(err.Error())
 	}
 
+	// 結果を変数に代入し、出力する
 	for rows.Next() {
 		var question_id  int
 		var question_name string
 		var question_text string
+
 		if err := rows.Scan(&question_id , &question_name , &question_text); err != nil {
 			panic(err.Error())
 		}
